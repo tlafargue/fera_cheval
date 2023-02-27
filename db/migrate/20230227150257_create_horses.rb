03a83e11a8c4d37fe70ef9e815ec57b485bde823
Thibault Lafargue <thibault.gplus@gmail.com>
2023-02-27 17:01:59 +0100

    Setup devise and create migrations

--- db/migrate/20230227150257_create_horses.rb
+++ db/migrate/20230227150257_create_horses.rb
@@ -1,7 +1,7 @@
 class CreateHorses < ActiveRecord::Migration[7.0]
   def change
     create_table :horses do |t|
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.string :name
       t.string :breed
       t.string :specialty
--- db/migrate/20230227150343_create_bookings.rb
+++ db/migrate/20230227150343_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.references :horse_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :horse, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
       t.string :total_price
